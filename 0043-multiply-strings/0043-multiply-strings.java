aeca81665a67d1f6fd9505ba8b8ee94e77ed0f31
Hamza khattabi <hamza.khattabi@uit.ac.ma>
2024-10-11 16:32:23 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        int result = Integer.valueOf(num1)*Integer.valueOf(num2);
+
+System.out.println(result);
+
+
+        return String.valueOf(result);
+
+    }
+}
