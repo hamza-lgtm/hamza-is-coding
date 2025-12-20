4c7a5694569011a6902db706c9e150d0ef85dee5
Hamza khattabi <hamza.khattabi@uit.ac.ma>
2025-12-20 19:27:31 +0100

    Time: 0 ms (100%), Space: 17.5 MB (99.88%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -9,7 +9,7 @@ class Solution:
         if not  root :
             return 0
         total = 0 
-        if root.left and not root.left.left:
+        if root.left and not root.left.left and not root.left.right:
             total += root.left.val
         total += self.sumOfLeftLeaves(root.left)
         total += self.sumOfLeftLeaves(root.right)
